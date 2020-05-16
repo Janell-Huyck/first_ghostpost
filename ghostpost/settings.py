3350a2343f38be6a8623364e48f5143cede1284d
Janell.Huyck <Janell.Huyck@outlook.com>
2020-05-15 23:32:09 -0400

    applied css

--- ghostpost/settings.py
+++ ghostpost/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'poster',
-    'django_filters',
-    'bootstrapform'
+    # 'django_filters',
+    # 'bootstrapform'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
